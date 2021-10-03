31e5ba8eefa3b9d932b20b76dd466f20ca32d52a
JoaquinS <joaquinsomoza6@gmail.com>
2021-10-03 19:53:44 -0300

    Desafio1 Joaquin Somoza C16860

---
+++ Desafio1/js/app.js
@@ -0,0 +1,15 @@
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+
+const espacio = " ";
+
+let nombreCompleto = "Bienvenido/a " + nombre + espacio + apellido;
+
+alert(nombreCompleto);
+
+let numeroA = parseInt(prompt("Ingrese un numero entero"));
+let numeroB = parseInt(prompt("Ingrese otro numero entero"));
+
+let resultado = numeroA + numeroB;
+
+alert("La suma de ambos numeros es: " + resultado);
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-Buenas! Acá van a estar todos los desafios en distintas ramas nombradas "Desafio número x (correspondiente a cada desafio)". 
+Buenas! Acá van a estar todos los desafios en distintas carpetas nombradas "Desafio número x (correspondiente a cada desafio)". Mi nombre es Joaquin Somoza y estoy en la Clase 16860 de JavaScript.
