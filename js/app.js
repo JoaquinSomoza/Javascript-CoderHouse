ab068ee2a3453887ce327c127ec205968b8d0e9e
JoaquinS <joaquinsomoza6@gmail.com>
2021-10-03 19:24:16 -0300

    Segundo desafio- Joaquin Somoza c16860

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Ingrese un número entero entre 1000 y 1050"));
+
+if ((numero > 999) && (numero < 1051)) {
+    alert("Bien, tu número se encuentra entre ellos");
+} else {
+    alert("Lo siento, tu número no se encuentra entre 1000 y 1050");
+}
