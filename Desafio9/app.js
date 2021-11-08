7f9c37d8ca4da7ebe6d9dd43d646ab615dca3e19
JoaquinS <joaquinsomoza6@gmail.com>
2021-11-08 14:01:12 -0300

    correcciones hechas en Desafio9

--- Desafio9/app.js
+++ Desafio9/app.js
@@ -1,54 +1,81 @@
-
 alert("Los días que se reservan turnos son los Lunes y Martes unicamente.");
 
-let contadorTurno = 0;
-let tiempoEstimado = 0;
 const turnos = [];
-let diaAlmacenado;
-let horaAlmacenada;
 
 
 let botonSelectDay = document.getElementById("selectDay");
-botonSelectDay.addEventListener("click", seleccionarDia);
+botonSelectDay.addEventListener("click", capturar);
 
-function seleccionarDia() {
-    let dia = prompt("Ingrese el día que desea reservar");
+let botonSelectHour = document.getElementById("selectHour");
+botonSelectHour.addEventListener("click", capturar);
+
+
+function capturar() {
+    let dia = document.getElementById("dia").value;
     if ((dia == "Lunes") || (dia == "Martes")) {
-        diaAlmacenado = dia;
+        let hora = document.getElementById("hora").value;
+        if ((hora >= 8) && (hora <= 18)) {
+            turnos.push({ "dia": dia, "hora": hora })
+            console.log(turnos);
+            let padre = document.getElementById("otorgados");
+            for (const turnosOcupados of turnos) {
+                let li = document.createElement("li");
+                li.innerHTML = `${turnosOcupados.dia}  ${turnosOcupados.hora} hs`
+                padre.appendChild(li);
+
+            }
+        } else {
+            alert("Los turnos se reservan a partir de las 8 horas, intentelo nuevamente.");
+        }
     } else {
         alert("El día que ingresó no es Lunes ni Martes, intentelo nuevamente.");
     }
-};
-let botonSelectHour = document.getElementById("selectHour");
-botonSelectHour.addEventListener("click", seleccionarHora);
+}
 
-function seleccionarHora() {
-    let hora = prompt("Ingrese la hora que desea reservar");
-    if ((hora >= 8) && (hora <= 18)) {
+var down = document.getElementById("GFG_DOWN");
+function GFG_Fun() {
 
-        horaAlmacenada = hora;
-    } else {
-        alert("Los turnos se reservan a partir de las 8 horas, intentelo nuevamente.");
-    }
-};
+    // Create a form synamically
+    var form = document.createElement("form");
 
-let botonDia = document.getElementById("btnDia");
-botonDia.addEventListener("click", mostrarDia);
-function mostrarDia() {
-    alert("Su dia seleccionado es el " + diaAlmacenado + ".");
-};
+    // Create an input element for emailID
+    var ID = document.createElement("input");
+    ID.setAttribute("type", "text");
+    ID.setAttribute("id", "dia");
+    ID.setAttribute("placeholder", "Dia");
 
-let botonHora = document.getElementById("btnHora");
-botonHora.addEventListener("click", mostrarHora);
-function mostrarHora() {
-    alert("Su hora seleccionada es a las " + horaAlmacenada + " horas.");
-};
+    // Create an input element for password
+    var PWD = document.createElement("input");
+    PWD.setAttribute("type", "text");
+    PWD.setAttribute("id", "hora");
+    PWD.setAttribute("placeholder", "Hora");
 
-let botonEspera = document.getElementById("btnEspera");
-botonEspera.addEventListener("click", mostrarEspera);
-function mostrarEspera() {
-    alert("Su tiempo de espera estimado es de " + tiempoEstimado + " minutos.");
-};
+    // Create a submit button
+    var s = document.createElement("input");
+    s.setAttribute("type", "button");
+    s.setAttribute("value", "Reservar");
+    s.setAttribute("onClick", "capturar()");
+
+
+    // Append the email_ID input to the form
+    form.append(ID);
+
+    // Append the password to the form
+    form.append(PWD);
+
+    // Append the button to the form
+    form.append(s);
+
+    document.getElementsByTagName("article")[0]
+        .appendChild(form);
+}
+
+
+//creacion del autor en footer.
+let primerFooter = document.createElement("div");
+primerFooter.innerHTML = "<h4>Joaquin Somoza</h4>";
+document.body.appendChild(primerFooter);
+
 
 
 
